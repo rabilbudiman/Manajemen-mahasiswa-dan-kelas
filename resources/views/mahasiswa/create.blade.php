f2fddd73f45d9d21dac069eb080d9e97d034282b
Awaludin <awalofficial17@gmail.com>
2023-06-22 15:03:22 +0700

    ngetes comid

--- resources/views/mahasiswa/create.blade.php
+++ resources/views/mahasiswa/create.blade.php
@@ -2,21 +2,6 @@
 <!-- START FORM -->
 @section('konten') 
 
-<nav class="py-2 bg-light border-bottom">
-    <div class="container d-flex flex-wrap">
-      <ul class="nav me-auto">
-        <li class="nav-item"><a href="#" class="nav-link link-dark px-2 active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">About</a></li>
-      </ul>
-      <ul class="nav">
-        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Logout</a></li>
-      </ul>
-    </div>
-  </nav>
-
 <form action='{{ url('mahasiswa') }}' method='post'>
 @csrf 
 <div class="my-3 p-3 bg-body rounded shadow-sm">
