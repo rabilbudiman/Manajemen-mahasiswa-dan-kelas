5e6f9ed92d2cf238bf10a7550b7ccbd1212c0d66
Rizki-Akbar-Pratama <201220100@unmuhpnk.ac.id>
2023-07-19 18:23:40 +0700

    kurang s

--- resources/views/mahasiswa/edit.blade.php
+++ resources/views/mahasiswa/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.template')
+@extends('layouts.template')
 <!-- START FORM -->
 @section('konten') 
 
--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.template')
+@extends('layouts.template')
 <!-- START DATA -->
 @section('konten')   
 
